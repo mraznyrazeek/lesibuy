268b4db2899bb5d43c396728e54014d0ab91a0d3
mraznyrazeek <mraznyrazeek@gmail.com>
2026-04-19 20:34:06 +0100

    Build the Intial login page

---
+++ lesibuy-admin/src/app/layouts/admin-layout/admin-layout.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-admin-layout',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './admin-layout.html',
+  styleUrl: './admin-layout.scss'
+})
+export class AdminLayoutComponent {
+  constructor(public authService: AuthService) {}
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
