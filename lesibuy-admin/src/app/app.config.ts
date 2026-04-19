749f73867a5144ec14c6c052fdcf2fd23301485c
mraznyrazeek <mraznyrazeek@gmail.com>
2026-04-19 20:17:43 +0100

    Setup the Angular agin without SSR Errors

--- lesibuy-admin/src/app/app.config.ts
+++ lesibuy-admin/src/app/app.config.ts
@@ -1,13 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
   ]
 };
